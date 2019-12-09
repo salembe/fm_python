67fe7f5f86d90f2330eee229dca0fa77ad21d427
余友 <yuyou@qutoutiao.net>
2019-12-09 15:52:16 +0800

    new

--- fm_python.py
+++ fm_python.py
@@ -1,11 +1,12 @@
-#coding:UTF-8
+# coding:UTF-8
 
 from __future__ import division
 from math import exp
 import numpy as np
 from numpy import *
-from random import normalvariate#ÕıÌ¬·Ö²¼
+from random import normalvariate  # æ­£æ€åˆ†å¸ƒ
 from datetime import datetime
+from sklearn.metrics import log_loss, roc_auc_score
 
 trainData = 'diabetes_train.txt'
 testData = 'diabetes_test.txt'
@@ -13,153 +14,187 @@ featureNum = 8
 max_list = []
 min_list = []
 
-def normalize(x_list,max_list,min_list):
+
+def normalize(x_list, max_list, min_list):
     index = 0
     scalar_list = []
     for x in x_list:
         x_max = max_list[index]
         x_min = min_list[index]
         if x_max == x_min:
-            x = 1.0    
+            x = 1.0
         else:
-            x = round((x-x_min)/(x_max-x_min),4)
+            x = round((x - x_min) / (x_max - x_min), 4)
         scalar_list.append(x)
         index += 1
     return scalar_list
+
 
 def loadTrainDataSet(data):
     global max_list
     global min_list
     dataMat = []
     labelMat = []
-    
-    fr = open(data)#´ò¿ªÎÄ¼ş
-    
+
+    fr = open(data)  # æ‰“å¼€æ–‡ä»¶
+
     for line in fr.readlines():
         currLine = line.strip().split(',')
-        #lineArr = [1.0]
+        # lineArr = [1.0]
         lineArr = []
-        
-        for i in xrange(featureNum):
+
+        for i in range(featureNum):
             lineArr.append(float(currLine[i]))
 
         dataMat.append(lineArr)
-        
+
         labelMat.append(float(currLine[-1]) * 2 - 1)
-    
+
     data_array = np.array(dataMat)
-    max_list = np.max(data_array,axis=0)
-    min_list = np.min(data_array,axis=0)
+    max_list = np.max(data_array, axis=0)
+    min_list = np.min(data_array, axis=0)
 
     scalar_dataMat = []
     for row in dataMat:
-        scalar_row = normalize(row,max_list,min_list)
+        scalar_row = normalize(row, max_list, min_list)
         scalar_dataMat.append(scalar_row)
     return scalar_dataMat, labelMat
+
 
 def loadTestDataSet(data):
     global max_list
     global min_list
     dataMat = []
     labelMat = []
-    
-    fr = open(data)#´ò¿ªÎÄ¼ş
-    
+
+    fr = open(data)  # æ‰“å¼€æ–‡ä»¶
+
     for line in fr.readlines():
         currLine = line.strip().split(',')
         lineArr = []
-        
-        for i in xrange(featureNum):
+
+        for i in range(featureNum):
             lineArr.append(float(currLine[i]))
 
         dataMat.append(lineArr)
-        
+
         labelMat.append(float(currLine[-1]) * 2 - 1)
-    
+
     data_array = np.array(dataMat)
 
     scalar_dataMat = []
     for row in dataMat:
-        scalar_row = normalize(row,max_list,min_list)
+        scalar_row = normalize(row, max_list, min_list)
         scalar_dataMat.append(scalar_row)
     return scalar_dataMat, labelMat
 
+
 def sigmoid(inx):
     return 1. / (1. + exp(-max(min(inx, 15.), -15.)))
-    #return 1.0 / (1 + exp(-inx))
+    # return 1.0 / (1 + exp(-inx))
+
 
 def stocGradAscent(dataMatrix, classLabels, k, iter):
-    #dataMatrixÓÃµÄÊÇmat, classLabelsÊÇÁĞ±í
+    # dataMatrixç”¨çš„æ˜¯mat, classLabelsæ˜¯åˆ—è¡¨
     m, n = shape(dataMatrix)
     alpha = 0.01
-    #³õÊ¼»¯²ÎÊı
-    #w = random.randn(n, 1)#ÆäÖĞnÊÇÌØÕ÷µÄ¸öÊı
+    # åˆå§‹åŒ–å‚æ•°
+    # w = random.randn(n, 1)#å…¶ä¸­næ˜¯ç‰¹å¾çš„ä¸ªæ•°
     w = zeros((n, 1))
     w_0 = 0.
     v = normalvariate(0, 0.2) * ones((n, k))
-    
-    for it in xrange(iter):
-        print it
-        for x in xrange(m):#Ëæ»úÓÅ»¯£¬¶ÔÃ¿Ò»¸öÑù±¾¶øÑÔµÄ
+
+    g_w_0 = 0
+    e = 0.00000001
+    r = 0.0001
+    g_w = [0] * len(w)
+    g_v = 0 * zeros((n, k))
+
+    def adagrad(_dw, _all_g):
+        return _dw / (math.sqrt(_all_g) + e)
+
+    for it in range(iter):
+        print(it)
+        for x in range(m):  # éšæœºä¼˜åŒ–ï¼Œå¯¹æ¯ä¸€ä¸ªæ ·æœ¬è€Œè¨€çš„
             inter_1 = dataMatrix[x] * v
-            inter_2 = multiply(dataMatrix[x], dataMatrix[x]) * multiply(v, v)#multiply¶ÔÓ¦ÔªËØÏà³Ë
-            #Íê³É½»²æÏî
+            inter_2 = multiply(dataMatrix[x], dataMatrix[x]) * multiply(v, v)  # multiplyå¯¹åº”å…ƒç´ ç›¸ä¹˜
+            # å®Œæˆäº¤å‰é¡¹
             interaction = sum(multiply(inter_1, inter_1) - inter_2) / 2.
-            
-            p = w_0 + dataMatrix[x] * w + interaction#¼ÆËãÔ¤²âµÄÊä³ö
-            #print "y: ",p 
+
+            p = w_0 + dataMatrix[x] * w + interaction  # è®¡ç®—é¢„æµ‹çš„è¾“å‡º
+            # print "y: ",p
             loss = sigmoid(classLabels[x] * p[0, 0]) - 1
-            #print "loss: ",loss
-        
-            w_0 = w_0 - alpha * loss * classLabels[x]
-            
-            for i in xrange(n):
+            # print "loss: ",loss
+
+            dw = loss * classLabels[x]
+            g_w_0 += (dw * dw)
+
+            # w_0 = w_0 - alpha * loss * classLabels[x] - alpha * r * w_0
+            w_0 = w_0 - alpha * adagrad(dw, g_w_0) * (dw + r * w_0)
+
+            for i in range(n):
                 if dataMatrix[x, i] != 0:
-                    w[i, 0] = w[i, 0] - alpha * loss * classLabels[x] * dataMatrix[x, i]
-                    for j in xrange(k):
-                        v[i, j] = v[i, j] - alpha * loss * classLabels[x] * (dataMatrix[x, i] * inter_1[0, j] - v[i, j] * dataMatrix[x, i] * dataMatrix[x, i])
-        
-    
+                    dw = loss * classLabels[x] * dataMatrix[x, i]
+                    g_w[i] += (dw * dw)
+                    # w[i, 0] = w[i, 0] - alpha * loss * classLabels[x] * dataMatrix[x, i] - alpha * r * w[i, 0]
+                    w[i, 0] = w[i, 0] - alpha * adagrad(dw, g_w[i]) * (dw + r * w[i, 0])
+                    for j in range(k):
+                        dw = loss * classLabels[x] * (
+                                dataMatrix[x, i] * inter_1[0, j] - v[i, j] * dataMatrix[x, i] * dataMatrix[x, i])
+                        g_v[i, j] += (dw * dw)
+
+                        # v[i, j] = v[i, j] - alpha * loss * classLabels[x] * (
+                        #         dataMatrix[x, i] * inter_1[0, j] - v[i, j] * dataMatrix[x, i] * dataMatrix[
+                        #     x, i]) - alpha * r * v[i, j]
+                        v[i, j] = v[i, j] - alpha * adagrad(dw, g_v[i, j]) * (dw + r * v[i, j])
+
+        # print('g_w_0=', g_w_0)
+        # print('g_w=', g_w)
+        # print('g_v=', g_v)
     return w_0, w, v
 
+
 def getAccuracy(dataMatrix, classLabels, w_0, w, v):
+    print('dataMatrix.shape=', dataMatrix.shape)
+    print('w shape=', w.shape)
+    print('v shape=', v.shape)
     m, n = shape(dataMatrix)
     allItem = 0
     error = 0
     result = []
-    for x in xrange(m):
+    for x in range(m):
         allItem += 1
         inter_1 = dataMatrix[x] * v
-        inter_2 = multiply(dataMatrix[x], dataMatrix[x]) * multiply(v, v)#multiply¶ÔÓ¦ÔªËØÏà³Ë
-        #Íê³É½»²æÏî
+        inter_2 = multiply(dataMatrix[x], dataMatrix[x]) * multiply(v, v)  # multiplyå¯¹åº”å…ƒç´ ç›¸ä¹˜
+        # å®Œæˆäº¤å‰é¡¹
         interaction = sum(multiply(inter_1, inter_1) - inter_2) / 2.
-        p = w_0 + dataMatrix[x] * w + interaction#¼ÆËãÔ¤²âµÄÊä³ö
-        
+        p = w_0 + dataMatrix[x] * w + interaction  # è®¡ç®—é¢„æµ‹çš„è¾“å‡º
+
         pre = sigmoid(p[0, 0])
-        
+
         result.append(pre)
-        
+
         if pre < 0.5 and classLabels[x] == 1.0:
             error += 1
         elif pre >= 0.5 and classLabels[x] == -1.0:
             error += 1
         else:
             continue
-        
-    
-    print result
-    
+
+    print(result)
+    print("AUC=", round(roc_auc_score(classLabels, result), 4))
+
     return float(error) / allItem
-        
-   
+
+
 if __name__ == '__main__':
     dataTrain, labelTrain = loadTrainDataSet(trainData)
     dataTest, labelTest = loadTestDataSet(testData)
     date_startTrain = datetime.now()
-    print "¿ªÊ¼ÑµÁ·"
-    w_0, w, v = stocGradAscent(mat(dataTrain), labelTrain, 20, 200)
-    print "ÑµÁ·×¼È·ĞÔÎª£º%f" % (1 - getAccuracy(mat(dataTrain), labelTrain, w_0, w, v))
+    print("å¼€å§‹è®­ç»ƒ")
+    w_0, w, v = stocGradAscent(mat(dataTrain), labelTrain, k=30, iter=20)
+    print("è®­ç»ƒå‡†ç¡®æ€§ä¸ºï¼š%f" % (1 - getAccuracy(mat(dataTrain), labelTrain, w_0, w, v)))
     date_endTrain = datetime.now()
-    print "ÑµÁ·Ê±¼äÎª£º%s" % (date_endTrain - date_startTrain)
-    print "¿ªÊ¼²âÊÔ"
-    print "²âÊÔ×¼È·ĞÔÎª£º%f" % (1 - getAccuracy(mat(dataTest), labelTest, w_0, w, v))  
+    print("è®­ç»ƒæ—¶é—´ä¸ºï¼š%s" % (date_endTrain - date_startTrain))
+    print("å¼€å§‹æµ‹è¯•")
+    print("æµ‹è¯•å‡†ç¡®æ€§ä¸ºï¼š%f" % (1 - getAccuracy(mat(dataTest), labelTest, w_0, w, v)))
